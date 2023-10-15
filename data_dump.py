2e3d654492fe4463183655e365b67719ac913f36
KALPESH NIKUMBH <kalpeshnikumbh811@gmail.com>
2023-10-15 17:05:38 +0530

    Data uploaded,Req folders created successfully.

--- data_dump.py
+++ data_dump.py
@@ -1,3 +1,26 @@
-import pymongo
+import pymongo # pip install pymongo
 import pandas as pd
-import json 
+import json
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://kalpnik8:knik@cluster0.azgxu7o.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+
+
+DATA_FILE_PATH = (r"C:\Users\kalpe\Desktop\Projects\Insurance-\insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
